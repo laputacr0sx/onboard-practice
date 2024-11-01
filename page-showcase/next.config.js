0ed11bff966dee74996697c52f5820fc6a69af5f
Felix Ng <laputafelix@gmail.com>
2024-11-01 11:54:46 +0800

    add cypress

--- page-showcase/next.config.js
+++ page-showcase/next.config.js
@@ -4,19 +4,19 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "images.hktv-img.com",
-        port: "",
-        pathname: "/images/HKTV/**",
+        protocol: 'https',
+        hostname: 'images.hktv-img.com',
+        port: '',
+        pathname: '/images/HKTV/**'
       },
       {
-        protocol: "https",
-        hostname: "placehold.co",
-        port: "",
-        pathname: "/**.png",
-      },
-    ],
-  },
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**.png'
+      }
+    ]
+  }
 };
 
 module.exports = nextConfig;
